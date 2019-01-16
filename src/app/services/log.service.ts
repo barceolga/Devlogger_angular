a1dd55f628176f9f2e80a10a4f3d3375cee4b436
Olga Furmanowska <olgafurmanowska@gmail.com>
2019-01-16 09:34:06 +0100

    Create funcionalities for clear, add, delete buttons

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -14,13 +14,18 @@ export class LogService {
   private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
   selectedLog = this.logSource.asObservable();
 
+  private stateSource = new BehaviorSubject<boolean>(true);
+  stateClear =this.stateSource.asObservable();
+
 
   constructor() {
-    this.logs = [
+    /*this.logs = [
       {id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:23')},
       {id: '2', text: 'Added Boostrap', date: new Date('12/27/2017 09:54:23')},
       {id: '3', text: 'Added logs components', date: new Date('12/27/2017 12:00:23')}
-    ]
+    ]*/
+
+    this.logs = [];
    }
 
    getLogs(): Observable<Log[]> {
@@ -30,4 +35,29 @@ export class LogService {
    setFormLog(log: Log) {
       this.logSource.next(log);
    }
+
+   addLog(log: Log) {
+     this.logs.unshift(log);
+   }
+
+   updateLog(log: Log) {
+     this.logs.forEach((cur, index) => {
+       if(log.id === cur.id) {
+         this.logs.splice(index, 1);
+       }
+     });
+     this.logs.unshift(log);
+   }
+
+    deleteLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if(log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+  }
+
+  clearState() {
+    this.stateSource.next(true);
+  }
 }
